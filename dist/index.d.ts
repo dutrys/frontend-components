a687ed79e0ae6ab6343f23ef236694708218b232
vaidas-dutrys <vaidas@dutrys.com>
2025-02-27 18:09:08 +0200

    add main

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,9 +1,2 @@
-export * from "./DatePicker";
-export * from "./DateTimePicker";
-export * from "./DateTimePicker";
-export * from "./Form";
-export * from "./Input";
 export * from "./Loading";
-export * from "./Popover";
-export * from "./SelectPaginatedFromApi";
 //# sourceMappingURL=index.d.ts.map
